fae2756f374638aedc98099f2fa46b1e34eb8e82
Rachel <yarkony.rachel@gmail.com>
2022-05-09 17:21:17 -0500

    added projects

--- rachel-react-portfolio/src/components/pages/Projects.js
+++ rachel-react-portfolio/src/components/pages/Projects.js
@@ -5,3 +5,44 @@
 // The title of the project
 // A link to the deployed application
 // A link to the corresponding GitHub repository
+import React from "react";
+
+function Projects() {
+  return (
+    <div className="projects-container">
+      <div className="container">
+        <div id="projectOne">
+          <a href="https://github.com/threemenandalady/project-two">
+            Scott's Tots
+          </a>
+        </div>
+        <div id="projectTwo">
+          <a href="https://github.com/The-camelCasers/project">
+            Do You Need an Umbrella?
+          </a>
+        </div>
+        <div id="projectThree">
+          <a href="https://github.com/buttercupsmom/social-network-startup">
+            Social Network Startup
+          </a>
+        </div>
+        <div id="projectFour">
+          {" "}
+          <a href="https://github.com/buttercupsmom/passwordgenerator">
+            Password Generator
+          </a>
+        </div>
+        <div id="projectFive">
+          <a href="https://github.com/buttercupsmom/codingquiz">
+            Buttercup Quiz
+          </a>
+        </div>
+        <div id="projectSix">
+          <a href="https://github.com/buttercupsmom/hw-6">Weather Dashboard</a>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Projects;
